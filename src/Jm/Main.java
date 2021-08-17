bc8c7dda33627d61269d53eb4d13d30c7a51ac77
Aleksandr Kozlov <A362132@yandex.ru>
2021-08-17 17:27:44 +0300

    Add new commit in master

--- src/Jm/Main.java
+++ src/Jm/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Create repository");
         System.out.println("Adding new commit");
         System.out.println("Add another commit");
+        System.out.println("Add commit in master");
     }
 }
